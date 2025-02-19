0d39ce759a0b309b1eaebb32b6d7b6e365fb1de5
MR-Hariharan <hariharankj24@gmail.com>
2025-02-19 15:45:41 +0530

    todays array program has been done successfully.

--- pattern2.java
+++ pattern2.java
@@ -5,7 +5,7 @@ public class pattern2 {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(count);
-                System.out.print(" ");
+              //  System.out.print(" ");
                 count++;
             }
         System.out.println("");
