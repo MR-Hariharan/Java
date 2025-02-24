28215ce341ed205639f97475d8e46b3844cfb880
MR-Hariharan <hariharankj24@gmail.com>
2025-02-24 10:26:46 +0530

    program in array

--- patterncross.java
+++ patterncross.java
@@ -1,15 +1,16 @@
 public class patterncross{
     public static void main(String args[]){
+    
         int n = 6;
-        for(int i=0;i<=n;i++) {
-            for(int j=0;j<=n;j++){
-                if(i==j||i+j==n){
-                System.out.print("*");
-            }else{
-                System.out.print(" ");
-            }
+        for(int i=1;i<n;i++){
+        for(int j=1;j<n;j++){
+        if (i==j||i+j==n){
+        System.out.print(i+" ");
+        }else{
+        System.out.print(" ");
         }
-            System.out.println();
         }
-    }
-}
+        System.out.println();
+        }
+        }
+        }
